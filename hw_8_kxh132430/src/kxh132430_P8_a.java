f5d0b27ed5c9219952af263ffd8f272a289c8938
KAI <KAI@KAI-THINK.gateway.pace.com>
2014-11-26 00:11:12 -0600

    I completed project 8 with comments

--- hw_8_kxh132430/src/kxh132430_P8_a.java
+++ hw_8_kxh132430/src/kxh132430_P8_a.java
@@ -5,187 +5,295 @@ import graph.In;
 import graph.IndexMinPQ;
 import graph.Queue;
 
+/**
+ * The kxh132430_P8_a class implements an algorithm to find the number 
+ * of simple shortest paths from a source node s to each node.
+ * The input is a directed graph G=(V,E) as input, with edge weights
+ * W:E-->Z (negative weights are possible).  The output is the number of
+ * shortest paths (not necessarily disjoint) from s to each vertex u in
+ * the graph.  If the graph has a negative or zero cycle, reachable from s,
+ * then print a message "Non-positive cycle in graph.  DAC is not applicable".
+ * 
+ * 
+ * Dependencies:	graph.DirectedEdge;
+ *					graph.EdgeWeightedDigraph;
+ *					graph.EdgeWeightedDirectedCycle;
+ *					graph.In;
+ *					graph.IndexMinPQ;
+ *					graph.Queue;
+ * 
+ * @version 	1.0 November 24th, 2014
+ * @author 		KAI HUANG              section 013
+ * 				VILAS REDDY PODDUTURI  section 016
+ *
+ */
 public class kxh132430_P8_a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		In in = new In("G:\\test\\p8-data\\channel-50-2.txt");
-		
+		//In in = new In("G:\\test\\p8-data\\channel-50-2.txt");
+
+		//Take input from stdin (console/standard input)
+		In in = new In();
+
+		// Read a sequence of lines to construct a directed graph
 		EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
 
+		// Test if the graph has a negative or zero cycle, reachable from s,
 		EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(G);
 
-		if(finder.hasCycle() == true){
+		// If there is a cycle, then print an error message.
+		if(finder.hasCycle()){
 			System.err.println("Non-positive cycle in graph. DAC is not applicable.");
 			System.exit(-1);
 		}
 
+		// Calculate the running time of the program
 		long startTime = 0, endTime = 0;
 
+		// If there is a negative-weighted edge in the graph, run BellmanFord algorithm.
 		if(G.hasNegativeEdge()){
-
 			startTime = System.currentTimeMillis();
 			BellmanFord bmf = new BellmanFord(G);
 			endTime = System.currentTimeMillis();
-
+			// print the length of a shortest path from s to t, the number of shortest paths
+			// from s to t, and the running time in msec.
 			bmf.printPath(G, (endTime - startTime));
-			
-		} else {
-			
+
+		} else {	// Otherwise run Dijkstra algorithm on the graph
 			startTime = System.currentTimeMillis();
 			Dijkstra dsp = new Dijkstra(G);
 			endTime = System.currentTimeMillis();
-
+			// print the length of a shortest path from s to t, the number of shortest paths
+			// from s to t, and the running time in msec.
 			dsp.printPath(G, (endTime - startTime));
-			
+
 		}
 	}
 }
 
+/**
+ * The Dijkstra class implements the Dijkstra algorithm on
+ * a single source directed graph with non-negative-weighted edge.
+ * 
+ * @author KAI
+ *
+ */
 class Dijkstra{
 
-	private long[] dist;        
-	int[] pred;
-	private long[] count;
-	private IndexMinPQ<Long> pq;
+	private long[] dist;   // The distance of each vertex to source     
+	int[] pred;			   // The predecessor of each vertex
+	private long[] count;  // The total number of shortest paths from source to each vertex
+	private IndexMinPQ<Long> pq; // A priority queue to store the vertices with the dist[u] as priority
 
+	/**
+	 * Dijkstra		A constructor that implements the Dijkstra algorithm.
+	 * @param G		The Directed Graph instance of the algorithm.
+	 */
 	Dijkstra(EdgeWeightedDigraph G){
+		// Initialization: 
 		int s = G.S();
 		dist = new long[G.V() + 1];
 		pred = new int[G.V() + 1];
 		count = new long[G.V() + 1];
 
-		// Initialization
+		// Set the distance of each vertex to s as infinity, except for s itself.
 		for (int u = 1; u <= G.V(); u++){
-			dist[u] = Long.MAX_VALUE;
+			dist[u] = Long.MAX_VALUE; // set dist[u] as infinity
 			count[u] = 0;
 		}
-		dist[s] = 0;
-		count[s] = 1;
-		// relax vertices in order of distance from s
+		dist[s] = 0;  // set dist[0] as 0
+		count[s] = 1; // There is one shortest path from s to s
+		
+		// Dijkstra algorithm:
+		// Process each vertex u on the priority queue in ascending order of the distance from s to u.
 		pq = new IndexMinPQ<Long>(G.V() + 1);
-		pq.insert(s, dist[s]);
+		pq.insert(s, dist[s]); // Insert s to the priority queue.
+		// Process the vertex on queue until the queue is empty.
 		while (!pq.isEmpty()) {
-			int u = pq.delMin();
+			int u = pq.delMin(); // Extract the vertex with the minimum distance to s from the priority queue.
 			for (DirectedEdge e : G.adj(u))
-				relax(e);
+				relax(e); // Relax all the edges incident to this vertex
 		}
 	}
 
-	// relax edge e and update pq if changed
+	/**
+	 * relax		Relax an edge (u,v) and update the priority queue 
+	 * 				if dist[u] + w(u,v) > dist[v]. 
+	 * @param e		The edge to be relaxed.
+	 */
 	private void relax(DirectedEdge e) {
 		int u = e.from(), v = e.to();
-		if (dist[v] > dist[u] + e.weight()) {
+		// If dist[v] needs to be updated
+		if (dist[v] > dist[u] + e.weight()) { 
 			dist[v] = dist[u] + e.weight();
-			pred[v] = u;
-			count[v] = count[u];
-			if (pq.contains(v)) 
+			pred[v] = u;  // Set u as the predecessor of v
+			count[v] = count[u];  // There are equal number of shortest paths from s to u and from s to v
+			if (pq.contains(v))  // If v is already on the priority queue, decreaseKey, adjust the index.
 				pq.decreaseKey(v, dist[v]);
 			else                
-				pq.insert(v, dist[v]);
-		} else if(dist[v] == dist[u] + e.weight()){
-			count[v] += count[u];
+				pq.insert(v, dist[v]); // Insert v to the priority queue if it is not on it.
+		} else if(dist[v] == dist[u] + e.weight()){ // If dist[v] is the minimum distance from s to v.
+			count[v] += count[u];  // Count[s->v] = Count[s->v] + Count[s->u->v]
 		}
 	}
 
-	public boolean hasPathTo(int u) {
+	/**
+	 * reachable	Test if vertex u is reachable from s.
+	 * @param u		The vertex to reach from s.		
+	 * @return		If u is reachable from s, return true, otherwise return false.
+	 */
+	public boolean reachable(int u) {
 		return dist[u] < Long.MAX_VALUE;
 	}
 
+	/**
+	 * hasPred		Test if u has a predecessor in the shortest path tree.
+	 * @param u		A given vertex in the graph.	
+	 * @return		If u has a predecessor, return true, otherwise return false.
+	 */
 	public boolean hasPred(int u) {
 		return pred[u] != 0;
 	}
 
+	/**
+	 * printPath		Output the lengths of shortest paths from s to each vertex u in the graph, 
+	 * 					the predecessor node of u in that shortest path, and the number of shortest 
+	 * 					paths from s to u. If there is no path from s to a vertex, print INF as the 
+	 * 					length, and - as the predecessor.
+	 * @param G			The graph instance on which the BellmanFord algorithm runs. 
+	 * @param time		The running time of the BellmanFord algorithm.
+	 */
 	public void printPath(EdgeWeightedDigraph G, long time){
-		// print shortest path
+		// Print the length of a shortest path from s to t, 
+		// the number of shortest paths from s to t, and the running time.
 		int t = G.T();
 		System.out.println(dist[t] + " " + count[t] + " " + time);
-
-		if(G.V() <= 100){
+		if(G.V() <= 100){ // Print the shortest path details if |V| is less than or equal to 100
 			for(int u = 1; u <= G.V(); u++){
-				String distance = hasPathTo(u)? String.valueOf(dist[u]) : "INF";
+				String distance = reachable(u)? String.valueOf(dist[u]) : "INF";
 				String predecessor = hasPred(u)? String.valueOf(pred[u]) : "-";
 				System.out.println(u + " " + distance + " " + predecessor + " " + count[u]);
 			}
 		}
 	}   
-	
+
 }
 
+/**
+ * The BellmanFord class implements the BellmanFord algorithm on
+ * a single source directed graph with negative-weighted edge allowed.
+ * 
+ * @author KAI
+ *
+ */
 class BellmanFord{
-	private long[] dist;              
-	private int[] pred;
-	private long[] count;
-	private boolean[] onQueue;             // onQueue[v] = is v currently on the queue?
-	private Queue<Integer> queue;          // queue of vertices to relax
-	private int[] num; // number of calls to relax()
+	private long[] dist; 	// The distance of each vertex to source             
+	private int[] pred; 	// The predecessor of each vertex
+	private long[] count;	// The total number of shortest paths from source to each vertex
+	private boolean[] inQueue; // A boolean value to record if a vertex is in the queue
+	private Queue<Integer> queue;  // A queue of vertices to be relaxed
+	private int[] num; // The number of relax operations on a given vertex
 
+	/**
+	 * BellmanFord	A constructor that implements the BellmanFord algorithm.
+	 * @param G		The Directed Graph instance of the algorithm.
+	 */
 	BellmanFord(EdgeWeightedDigraph G){
-
-		//	Intialization
+		
+		// Initialization: 
 		int s = G.S();
 		dist = new long[G.V() + 1];
-		pred = new int[G.V() + 1];
+		pred = new int[G.V() + 1]; // Set the predecessor of each vertex as 0.
 		count = new long[G.V() + 1];
 		num = new int[G.V() + 1];
-		onQueue = new boolean[G.V() + 1];
+		inQueue = new boolean[G.V() + 1];
+		
+	    // Set the distance of each vertex to s as infinity, except for s itself.
 		for (int u = 1; u <= G.V(); u++){
-			dist[u] = Long.MAX_VALUE;
-			count[u] = 0;
+			dist[u] = Long.MAX_VALUE; 
+			count[u] = 0;  // Set the number of shortest path from source to each vertex as 0.
 		}
-		dist[s] = 0;
-		count[s] = 1;
+		dist[s] = 0;  // Set the distance of s to s as 0.
+		count[s] = 1;  // There is one path from s to s.
 
 		// Bellman-Ford algorithm
+		// Create a queue to store the vertices to be relaxed, enqueue s.
 		queue = new Queue<Integer>();
 		queue.enqueue(s);
-		onQueue[s] = true;
+		inQueue[s] = true;
+		// Relax the vertex on queue until the queue is empty.
 		while (!queue.isEmpty()) {
 			int u = queue.dequeue();
-			if((num[u]++) >= G.V()){
+			if((num[u]++) >= G.V()){ // Check if the graph has a non-positive cycle.
 				throw new RuntimeException("Non-positive cycle in graph. DAC is not applicable.");
 			}
-			onQueue[u] = false;
-			relax(G, u);
+			inQueue[u] = false;
+			relax(G, u); // Relax a given vertex u
 		}
 	}
 
-	// relax vertex v and put other endpoints on queue if changed
-	private void relax(EdgeWeightedDigraph G, int u) {
+	/**
+	 * relax		Relax a given vertex u, update its neighbor v and
+	 * 				put vertex v on queue, if dist[u] + w(u,v) < dist[v].
+	 * @param G		The graph that contains all neighbors of vertex u in adj[u].
+	 * @param u		The vertex u to be relaxed.
+	 */
+	public void relax(EdgeWeightedDigraph G, int u) {
+		
+		// For each neighbor v of u, relax them and put them on queue if dist[v] is updated.
 		for (DirectedEdge e : G.adj(u)) {
 			int v = e.to();
-
+			// If dist[v] needs to be updated
 			if (dist[v] > dist[u] + e.weight()) {
-				dist[v] = dist[u] + e.weight();
-				pred[v] = u;
-				count[v] = count[u];
-				if (!onQueue[v]) {
-					queue.enqueue(v);
-					onQueue[v] = true;
+				dist[v] = dist[u] + e.weight(); // Update dist[v]
+				pred[v] = u; // Set u as the predecessor of v
+				count[v] = count[u]; // There are equal number of shortest paths from s to u and from s to v
+				if (!inQueue[v]) { // If v is not in queue, enqueue v.
+					queue.enqueue(v); 
+					inQueue[v] = true;
 				} 
-			} else if(dist[v] == dist[u] + e.weight()){
-				count[v] += count[u];
+			} else if(dist[v] == dist[u] + e.weight()){ // If dist[v] is the minimum distance from s to v.
+				count[v] += count[u]; // Count[s->v] = Count[s->v] + Count[s->u->v]
 			}
-
 		}
 	}
 
-	public boolean hasPathTo(int u) {
+	/**
+	 * reachable	Test if vertex u is reachable from s.
+	 * @param u		The vertex to reach from s.		
+	 * @return		If u is reachable from s, return true, otherwise return false.
+	 */
+	public boolean reachable(int u) {
 		return dist[u] < Long.MAX_VALUE;
 	}
 
+	/**
+	 * hasPred		Test if u has a predecessor in the shortest path tree.
+	 * @param u		A given vertex in the graph.	
+	 * @return		If u has a predecessor, return true, otherwise return false.
+	 */
 	public boolean hasPred(int u) {
 		return pred[u] != 0;
 	}
 
+	/**
+	 * printPath		Output the lengths of shortest paths from s to each vertex u in the graph, 
+	 * 					the predecessor node of u in that shortest path, and the number of shortest 
+	 * 					paths from s to u. If there is no path from s to a vertex, print INF as the 
+	 * 					length, and - as the predecessor.
+	 * @param G			The graph instance on which the BellmanFord algorithm runs. 
+	 * @param time		The running time of the BellmanFord algorithm.
+	 */
 	public void printPath(EdgeWeightedDigraph G, long time){
-		// print shortest path
+		// Print the length of a shortest path from s to t, 
+		// the number of shortest paths from s to t, and the running time.
 		int t = G.T();
 		System.out.println(dist[t] + " " + count[t] + " " + time);
-		if(G.V() <= 100){ // Print only if V >= 100
+		if(G.V() <= 100){ // Print the shortest path details if |V| is less than or equal to 100
 			for(int u = 1; u <= G.V(); u++){
-				String distance = hasPathTo(u)? String.valueOf(dist[u]) : "INF";
+				String distance = reachable(u)? String.valueOf(dist[u]) : "INF";
 				String predecessor = hasPred(u)? String.valueOf(pred[u]) : "-";
 				System.out.println(u + " " + distance + " " + predecessor + " " + count[u]);
 			}
--- hw_8_kxh132430/src/kxh132430_P8_b.java
+++ hw_8_kxh132430/src/kxh132430_P8_b.java
@@ -6,218 +6,340 @@ import graph.EdgeWeightedDigraph;
 import graph.In;
 import graph.Queue;
 
+/**
+ * The kxh132430_P8_b class implements an algorithm to solve the reward collection problem.
+ * The input is an undirected graph G=(V,E), with weights W:E-->Z+, a
+ * source node s in V.  In addition each node has a reward amount
+ * associated with it, R:V-->Z+.  This amount can be claimed by visiting
+ * that node (representing a node that is dirty and cleaning it with a
+ * vacuum cleaner).  Starting from s, find a traversal that starts and
+ * ends at s, in which the reward collected is a maximum.
+ * 
+ * Dependencies:	graph.DirectedEdge;
+ *					graph.EdgeWeightedDigraph;
+ *					graph.In;
+ *					graph.Queue;
+ *					kxh132430_P8_a
+ *
+ * @version 	1.0 November 24th, 2014
+ * @author 		KAI HUANG              section 013
+ * 				VILAS REDDY PODDUTURI  section 016
+ * 
+ */
 public class kxh132430_P8_b {
 
 	public static void main(String[] args) {
 
-		In in = new In("G:\\test\\p8-data\\p8-partb\\test.txt");
+		//In in = new In("G:\\test\\p8-data\\p8-partb\\test.txt");
 
-		EdgeWeightedGraph G2 = new EdgeWeightedGraph(in);
+		//Take input from stdin (console/standard input)
+		In in = new In();
+		
+		// Read a sequence of lines to construct an undirected graph
+		EdgeWeightedGraph G = new EdgeWeightedGraph(in);
 
+		// If there is a negative-weighted edge, then print an error message.
+		if(G.hasNegativeEdge()){
+			System.err.println("Negative-weighted edge in graph.");
+			System.exit(-1);
+		}
+		// Calculate the running time of the program.
 		long startTime = 0, endTime = 0;
 		startTime = System.currentTimeMillis();
-		RewardCollection rc = new RewardCollection(G2);
+		RewardCollection rc = new RewardCollection(G); // Run Reward Collection algorithm on this instance.
 		endTime = System.currentTimeMillis();
-		
-		rc.printPath(G2, (endTime - startTime));
-		
+		// print the length of a shortest path from s to t, the number of shortest paths
+		// from s to t, and the running time in msec.
+		rc.printPath(G, (endTime - startTime));
+
 	}
 
 }
 
+/**
+ * The RewardCollection class implements the Dijkstra algorithm on
+ * a single source directed graph with non-negative-weighted edge.
+ * 
+ * @author KAI
+ *
+ */
 class RewardCollection{
-	
-	private long maxCost;
-	private ArrayList<Integer> maxPath;
-	private int[] pred;
-	
+
+	private long maxCost; // The maximum reward amount we can get by traversing the graph
+	private ArrayList<Integer> maxPath;	// The traversing path to get the maximum reward amount.
+	private int[] pred; // The predecessor of each vertices on the shortest path tree.
+
+	/**
+	 * RewardCollection		A constructor that implements the RewardCollection algorithm.
+	 * @param G				The Undirected graph instance of the algorithm.
+	 */
 	RewardCollection(EdgeWeightedGraph G){
 
+		// Run a Dijkstra algorithm on this instance
 		Dijkstra dsp = new Dijkstra(G);
+		// Let pred be the array that records the predecessor of each vertices on the shortest path tree.
 		pred = dsp.pred;
-		dsp = null;
-		
+		dsp = null; // Free the memory space
+
+		// Run a Breadth First Search on the instance. Getting a collection of
+		// tours that starts and ends at s, and visit each vertices except s only once.
 		BFS bfs = new BFS();
 		ArrayList<ArrayList<Integer>> tourSet = bfs.BFS_Search(G);
-		bfs = null;
-		
+		bfs = null; // Free the memory space
+
+		// Let maxPath be the tour to collect the maximum rewards
 		maxPath = new ArrayList<Integer>(); 
 
+		// Iterate through each tour in the collection of all valid tours in the graph starting and ending at s.
 		Iterator<ArrayList<Integer>> itr = tourSet.iterator();
-
+		
 		while(itr.hasNext()){
-			
-			ArrayList<Integer> tour = itr.next();
-			
-			long pathCost = 0;
+			ArrayList<Integer> tour = itr.next(); // Get one tour from the collection of all valid tours
+			long pathCost = 0; // Let pathCost be the reward amount collected along the tour.
 			int u = G.S(); 
-			
+			// Iterate through each vertices on this tour
 			Iterator<Integer> it = tour.iterator();
 			while(it.hasNext()){
 				int v = it.next();
-				if(pred[v] == u){
-					pathCost += G.cost[v];
+				if(pred[v] == u){ // If (u,v) is also on the shortest path tree
+					pathCost += G.cost[v]; // Add the reward amount at v to pathCost
 				}
 				u = v;
 			}
-
+			// Update the maximum cost and the corresponding tour if needed
 			if(pathCost > maxCost){
 				maxCost = pathCost;
 				maxPath = tour;
 			}
 		}
-	
+
 	}
-	
+
+	/**
+	 * printPath		In the first line of the output, print the maximum reward that can be collected, 
+	 * 					and the running time. In the next lines, print the nodes in the traversal, in the order
+	 *					visited, and the reward collected at that node.
+	 *
+	 * @param G			The graph instance on which the RewardCollection algorithm runs. 
+	 * @param time		The running time of the RewardCollection algorithm.
+	 */
 	public void printPath(EdgeWeightedGraph G, long time){
-		
+
+		// In the first line of the output, print the maximum reward 
+		// that can be collected, and the running time.
 		System.out.println(this.maxCost + " " + time);
-		System.out.println(G.S() + " " + G.cost[G.S()]);
 		
+		// In the next lines, print the nodes in the traversal, in the order
+		// visited, and the reward collected at that node.
+		System.out.println(G.S() + " " + G.cost[G.S()]);
+		// Iterate through the maxPath that collects the maximum rewards amount.
 		Iterator<Integer> it = this.maxPath.iterator();
 		int u = G.S();
 		while(it.hasNext()){
 			int v =it.next();
-			long cost = 0;
-			if(this.pred[v] == u){
-				cost = G.cost[v];
+			long cost = 0; // The reward amount is zero if v is not reached via the shortest path
+			if(this.pred[v] == u){ // If (u,v) is also on the shortest path tree
+				cost = G.cost[v]; // Collect the reward
 			} 
 			System.out.println(v + " " + cost);
 			u = v;
 		}
 	}
-	
+
 }
 
+/**
+ * The EdgeWeightedGraph class extends the EdgeWeightedDigraph class
+ * to represent an undirected graph.
+ * 
+ * @author KAI
+ *
+ */
 class EdgeWeightedGraph extends EdgeWeightedDigraph{
-	
-	public int[] cost;
-	
-	EdgeWeightedGraph(int v) {
-		super(v);
-		this.cost = new int[v+1];
-	}
-	
-	EdgeWeightedGraph(In in) {
-    	this(in.readInt());
-        int E = in.readInt();
-        if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
-        this.S = in.readInt();
-        
-        for(int i = 1; i<= V; i++){
-        	cost[i] = in.readInt();
-        }
-        
-        if(E > 100000) 
-        	E = 100000;
-        for (int i = 1; i <= E; i++) {
-            
-        	int u = in.readInt();
-            
-            int v = in.readInt();
-            
-            if (u <= 0 || u > V) throw new IndexOutOfBoundsException("vertex " + u + " is not between 1 and " + V);
-            
-            if (v <= 0 || v > V) throw new IndexOutOfBoundsException("vertex " + v + " is not between 1 and " + V);
 
-            int w = in.readInt();
-            
-            if(w < 0) {
-            	this.negativeEdge = true;
-            }
-            
-            addEdge(new DirectedEdge(u, v, w));
-            addEdge(new DirectedEdge(v, u, w));
-        }
-    }
+	public int[] cost; // The reward amount associated with each vertex.
+
+	EdgeWeightedGraph(int v) {
+		super(v);  // Inherit the constructor from Directed Graph class
+		this.cost = new int[v+1]; 
+	}
+
+	EdgeWeightedGraph(In in) {
+		this(in.readInt());  // Let V be the number of vertex
+		int E = in.readInt();  // Let E be the number of edges
+		if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+		this.S = in.readInt(); // Let S be the source
+
+		// In the next V lines, read the reward amount at each vertices.
+		for(int i = 1; i<= V; i++){
+			cost[i] = in.readInt(); 
+		}
+
+		// If E is greater than 100000, only read the first 100000 edges from inputs
+		if(E > 100000) 
+			E = 100000;
+		
+		// Get the two vertices of an edge from the input
+		for (int i = 1; i <= E; i++) {
+			int u = in.readInt();
+			int v = in.readInt();
+
+			if (u <= 0 || u > V) throw new IndexOutOfBoundsException("vertex " + u + " is not between 1 and " + V);
+			if (v <= 0 || v > V) throw new IndexOutOfBoundsException("vertex " + v + " is not between 1 and " + V);
+
+			// Check if there is a negative-weighted edge.
+			int w = in.readInt();
+			if(w < 0) {
+				this.negativeEdge = true; 
+			}
+
+			// Add both edge(u,v) and edge(v,u) to the graph
+			addEdge(new DirectedEdge(u, v, w));
+			addEdge(new DirectedEdge(v, u, w));
+		}
+	}
 }
 
+/**
+ * The Node class implements the Node data structure in Breadth First Search
+ * @author KAI
+ *
+ */
 class Node{
-	
-	Node parent;
-	State state;
-	
+
+	Node parent; // The parent of this node in BFS
+	State state; // The state of this node
+
 	Node(Node parent, State state){
 		this.parent = parent;
 		this.state = state;
 	}	
 }
 
+/**
+ * The State class implements the State of a Node in Breadth First Search
+ * @author KAI
+ *
+ */
 class State{
-	
-	int location;
-	ArrayList<Integer> path;
-	
+
+	int location;  // The location of the agent
+	ArrayList<Integer> path; // The path traversed so far
+
+	/**
+	 * A constructor that builds the INITIAL_STATE of the Breadth-First Search
+	 * @param location
+	 */
 	State(int location){
 		this.location = location;
 		this.path = new ArrayList<Integer>();
 	}
-	
+
+	/**
+	 * A constructor that constructs a state from an existing location and path
+	 * @param location
+	 * @param path
+	 */
 	State(int location, ArrayList<Integer> path){
 		this.location = location;
 		this.path = path;
 	}
 	
+	/**
+	 * A constructor that constructs a state from an existing state
+	 * @param state
+	 */
 	State(State state){
 		this.location = state.location;
 		this.path = state.path;
 	}
 }
 
+/**
+ * The BFS class implements a Breadth First Search on an undirected Graph
+ * @author KAI
+ *
+ */
 class BFS{
-	
+
+	/**
+	 * BFS_Search		Breadth First Search on an undirected Graph
+	 * @param G			The undirected graph instance
+	 * @return			A collection of tours starts and ends at s,
+	 * 					and visits each vertex on the path except s only once.
+	 */
 	public ArrayList<ArrayList<Integer>> BFS_Search(EdgeWeightedGraph G){
-		
+
+		// Let solution be the collection of all valid tours on the graph
 		ArrayList<ArrayList<Integer>> solution = new ArrayList<ArrayList<Integer>>();
-		
+
+		//Create the initial state in BFS
 		State INITIAL_STATE = new State(G.S());
 		
+		// Create a node with this initial state in BFS
 		Node node = new Node(null, INITIAL_STATE);
-		
+
+		// Create a queue to keep nodes for BFS
 		Queue<Node> queue = new Queue<Node>();
 		queue.enqueue(node);
 		
+		// Create an explored set to record the explored states
 		HashSet<State> explored = new HashSet<State>();
-		
+
+		// Expanding the nodes until the queue is empty
 		while (!(queue.isEmpty())) {
-			node = queue.dequeue();
-			explored.add(node.state);
-			//System.out.println("now exanding:" + node.state.location);
-			
+			node = queue.dequeue(); // Get one node from the queue
+			explored.add(node.state); // Add this node to explored set
+
 			int u = node.state.location;
-			
 			for(DirectedEdge e : G.adj(u)){
-				
+				// If the vertex is not on the prior tour path
 				if(!(node.state.path.contains(e.to()))){
+					// Expand the node by following edge d to generate a child node
 					Node childNode = RESULT(node, e);
-					if(!(explored.contains(childNode.state))){
-						if(GOAL_TEST(G, childNode.state)){
-							solution.add(childNode.state.path);
-						} else{
-							queue.enqueue(childNode);
+					if(!(explored.contains(childNode.state))){ // If the state of child node is not explored before
+						if(GOAL_TEST(G, childNode.state)){ // Test if we reach the goal state
+							solution.add(childNode.state.path); // Add this tour path to the solution
+						} else{ 
+							queue.enqueue(childNode); // Otherwise enqueue the child node
 						}
 					}
 				}
 			}
 		}
-		return solution;
+		return solution; 
 	}
-	
+
+	/**
+	 * RESULT			Generate a child node by expanding the parent node 
+	 * @param node		The parent node to be expanded
+	 * @param e			The edge connecting the parent and child node
+	 * @return			The child node in BFS
+	 */
 	public Node RESULT(Node node, DirectedEdge e){
-		
-		int v = e.to();
-		
+		// Let v be the child of u
+		int v = e.to(); 
+
+		// Append vertex v to the tour
 		ArrayList<Integer> path = new ArrayList<Integer>(node.state.path);
 		path.add(v);
-		
+
+		//Create a new state with this new path and new vertex v
 		State newState = new State(v, path);
-		
+
+		//Create a new node with this new state
 		Node childNode = new Node(node, newState);
-		
-		return childNode;
+
+		return childNode; // Return the child node
 	}
 	
+	/**
+	 * GOAL_TEST		Test if the BFS reaches the goal state
+	 * @param G			The graph instance of the BFS
+	 * @param state		The state to be tested
+	 * @return			return true if the BFS reaches the goal state, otherwise return false;
+	 */
 	public boolean GOAL_TEST(EdgeWeightedGraph G, State state){
-	    return state.location == G.S();
+		return state.location == G.S();
 	}
 }
