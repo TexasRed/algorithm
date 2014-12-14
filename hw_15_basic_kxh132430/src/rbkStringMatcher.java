91e1081242c9074a6036a6d16bfb8c7bdf6ad136
KAI <KAI@10.21.21.4>
2014-12-13 18:26:28 -0600

    I commented the code for project_15_basic

--- hw_15_basic_kxh132430/src/rbkStringMatcher.java
+++ hw_15_basic_kxh132430/src/rbkStringMatcher.java
@@ -52,7 +52,7 @@ public class rbkStringMatcher {
 			T += in.readLine();
 		}
 		in.close();
-		
+
 		// If the pattern string exceeds the text string, print error message
 		if(T.length() < P.length()){
 			System.err.println("The pattern string cannot exceeds the text string!");
@@ -96,7 +96,30 @@ public class rbkStringMatcher {
 
 	}
 
-	// Section 1: Naive algorithm
+    /*
+     * ********************************************************************
+     *   Helper function to calculate the running time
+     * ********************************************************************
+     */
+	static long startTime, endTime, elapsedTime;
+	static int phase = 0;
+
+	static void rbkTimer() {
+		if(phase == 0) { startTime = System.currentTimeMillis();  phase = 1; }
+		else {
+			endTime = System.currentTimeMillis();  
+			elapsedTime = endTime-startTime;
+			System.out.print(elapsedTime + " ");
+			phase = 0;
+		}
+	}
+	
+	
+	/*
+	 *************************************************
+	 *    	 Section 1: naive algorithm
+	 *************************************************
+	 */
 	/**
 	 * NaiveMatch	Given a text and a pattern, find all the valid shift that 
 	 * 				matches the pattern with the text using naive algorithm.
@@ -107,27 +130,33 @@ public class rbkStringMatcher {
 	static ArrayList<Integer> NaiveMatch(String T, String P){
 		int n = T.length(); // The length of text
 		int m = P.length(); // The length of pattern
-
+		// Let result be a list of all the valid shifts
 		ArrayList<Integer> result = new ArrayList<Integer>();
 
+		// Shift from left to right one character a time, compare pattern with
+		// all the m-grams in the text
 		for(int i = 0; i <= n - m; i++){
 			int j = 0;
+			// If T[i] matches P[j], move to the next character and compare again
 			while(j < m && T.charAt(i + j) ==P.charAt(j)){
 				j = j + 1;
 			}
-			if(j == m){
+			if(j == m){ // If all character matches, add this m-gram into result list
 				result.add(i);
 			}
 		}
 		return result;
 	}
 
-	
-	// Section 2: Rabin-Karp algorithm
-	
+
+	/*
+	 *************************************************
+	 *    	 Section 2: Rabin-Karp algorithm
+	 *************************************************
+	 */
 	// Generate a random 63-bit prime for the hash function
 	static long q = BigInteger.probablePrime(63, new Random()).longValue();
-	
+
 	/**
 	 * RKMatch		Given a text and a pattern, find all the valid shift that 
 	 * 				matches the pattern with the text using Rabin-Karp algorithm.
@@ -141,45 +170,35 @@ public class rbkStringMatcher {
 		// Let result be a list of all the valid shifts
 		ArrayList<Integer> result = new ArrayList<Integer>(); 
 
-		long h = (1 << m - 1 ) % q;
+		// Let multiplier be the maginitude of the MSD in the m-gram in text (mod q)
+		long multiplier = (1 << m - 1 ) % q;
 
-		//long[] t = new long[n - m + 1];
-
-		long p = 0;
-		//t[0] = 0;
-		long t = 0;
+		long p = 0;  // let p be the value of h[P[0...m-1]]
+		long t = 0; // let t be the value of h[T[0...m-1]];
 
 		for(int i = 0; i < m; i++){
-			p = ((p * 2) + (P.charAt(i) -'0')) % q;
-			//t[0] = ((t[0] * 2) + (T.charAt(i) - '0')) % q;
+			// Convert the pattern string P[0...m-1] into a decimal number (mod q)
+			p = ((p * 2) + (P.charAt(i) -'0')) % q;   
+			// Convert the m-gram T[0...m-1] into a decimal number (mod q)
 			t = ((t * 2) + (T.charAt(i) - '0')) % q;
-			//System.out.println(h + " " + p + " " + t[0] +" "+ q);
 		}
 
-
+		// Shift from left to right one character a time, compare pattern with
+		// all the m-grams in the text
 		for(int s = 0; s <= n-m; s++){
-
-			//if(p == t[s]){
-			if(p == t){
-				if(isMatch(T, P, s)){
+			if(p == t){ // If pattern and a m-gram in text hashes to the same slot in the hash table
+				if(isMatch(T, P, s)){ // Add this m-gram to result list if all characters of it match with the pattern
 					result.add(s);
 				}
 			}
-			if(s <= n - m - 1){
-				/*
-				t[s+1] = ((t[s] - (T.charAt(s) - '0')* h) * 2 + (T.charAt(s+m)-'0')) % q;
-				if(t[s+1] < 0){
-					t[s+1] = t[s+1] + q;
-				}
-				//System.out.println("T["+(s+1)+"]="+t[s+1]);
-				/*/
-				t = ((t - (T.charAt(s) - '0')* h) * 2 + (T.charAt(s+m)-'0')) % q;
+			if(s <= n - m - 1){ // If the text has a next m-gram
+				// compute the next m-gram T[s+1...s+m] from the current m-gram T[s...s+m-1]
+				t = ((t - (T.charAt(s) - '0')* multiplier) * 2 + (T.charAt(s+m)-'0')) % q;
 				if(t < 0){
-					t = t + q;
+					t = t + q;  // Normalize the remainder into positive value
 				}
 			}
 		}
-
 		return result;
 
 	}
@@ -195,30 +214,25 @@ public class rbkStringMatcher {
 	 * 				otherwise return false
 	 */
 	static boolean isMatch(String T, String P, int s){
-
+		// test if the m-gram T[s...s+m-1] in the text matches the pattern
 		int m = P.length();
-		
-		return T.substring(s, s + m -1).equals(P);
-		
+		return T.substring(s, s + m -1).equals(P); 
 	}
 
 	/**
-	 * isPrime 		Test if q is a prime number
+	 * @Deprecated  Validate if q is a prime number
 	 * @param q		number for the prime test
 	 * @return		return true is q is a prime number
 	 */
-	static boolean isPrime(int q){
-
+	static boolean isPrime(long q){
 		boolean prime = true;
-
+		
 		// 0 and 1 are not prime number
 		if(q == 0 || q == 1) { 
 			return false;
 		}
-
-		long r = (long)Math.sqrt(q);
-		
 		// If q has no divisor between 2 and sqrt(q), then q is a prime number
+		long r = (long)Math.sqrt(q);
 		for(long i = 2; i <= r; i++){
 			if(q % i == 0){
 				prime = false;
@@ -228,8 +242,11 @@ public class rbkStringMatcher {
 		return prime;
 	}
 
-	// Section 3: KMP Matcher
-
+	/*
+	 *************************************************
+	 *    Section 3: Knuth-Morris-Pratt algorithm
+	 *************************************************
+	 */
 	/**
 	 * KMPMatch		Given a text and a pattern, find all the valid shift that matches 
 	 * 				the pattern with the text using Knuth-Morris-Pratt algorithm.
@@ -238,41 +255,41 @@ public class rbkStringMatcher {
 	 * @return		a list of all the valid shifts
 	 */
 	static ArrayList<Integer> KMPMatch(String T, String P){
-		
+
 		int n = T.length(); // The length of text
 		int m = P.length(); // The length of pattern
-		
+
 		// Let pre be the prefix function
 		int[] pre = compute_prefix_function(P); 
 
 		// Let result be a list of all the valid shifts
 		ArrayList<Integer> result = new ArrayList<Integer>();
 
-		int q = 0;  // length of the matched characters in the text 
+		int q = 0;  // length of the matched characters in an m-gram and pattern 
 
 		// Scan the text from left to right
 		for(int i = 0; i < n; i++){
 			// If pattern and text does not match at the current character,
-			// but matches at certain index before the current character
-			// 
+			// but matches at some index before the current character
 			while(q > 0 && P.charAt(q) != T.charAt(i)){
-				q = pre[q];
+				q = pre[q];  // shift pattern by a amount such that its prefix
+							 // matches the suffix that matched the text so far
 			}
 			// If the pattern and text match at the current index, 
 			// increment the length of matched characters by one.
 			if(P.charAt(q) == T.charAt(i)){
 				q = q + 1;
 			}
-			// If the substring in
+			// If the all character matches in the m-gram and pattern
 			if(q == m){
-				result.add( i - m + 1);
-				q = pre[q];
+				result.add( i - m + 1); // add the shift index to the result list
+				q = pre[q]; // shift pattern by a amount such that its prefix
+				 			// matches the suffix that matches the text so far
 			}
 		}
-
 		return result;
 	}
-	
+
 	//  Helper function for KMP algorithm
 	/**
 	 * compute_prefix_function	Given a pattern, compute the maximum length of prefix that
@@ -281,14 +298,14 @@ public class rbkStringMatcher {
 	 * @return					The prefix function for each index in the pattern
 	 */
 	static int[] compute_prefix_function(String P){
-		
+
 		int m = P.length(); // The length of pattern
 
 		int[] pre = new int[m + 1];	// An array to store the prefix function
-		
+
 		// No prefix exists for empty string and string of length one
 		pre[0] = pre[1] = 0;  
-		
+
 		int k = 0;
 
 		for(int q = 1; q < m; q++){
@@ -303,11 +320,14 @@ public class rbkStringMatcher {
 		}
 
 		return pre;
-
 	}
 
 
-	// Section 4: Boyer-Moore algorithm
+	/*
+	 *************************************************
+     *	 Section 4: Boyer-Moore algorithm            *
+	 *************************************************
+	 */
 	/**
 	 * BMMatch		Given a text and a pattern, find all the valid shift that 
 	 * 				matches the pattern with the text using Boyer-Moore algorithm.
@@ -315,31 +335,35 @@ public class rbkStringMatcher {
 	 * @param P		pattern string
 	 * @return		a list of all the valid shift
 	 */
-	static ArrayList<Integer> BMMatch(String T, String P){
+	 static ArrayList<Integer> BMMatch(String T, String P){
 		int n = T.length(); // The length of text
 		int m = P.length(); // The length of pattern
 		// Let result be a list of all the valid shifts
 		ArrayList<Integer> result = new ArrayList<Integer>();
 
+		// Let i be the last index of an m-gram in the text, 
+		// Let j be the last index of the pattern
 		int i = m - 1;
 
-		if(i > n - 1){
+		if(i > n - 1){ // Return empty if pattern exceeds length of text
 			return null;
 		}
 		int j = m - 1;
 
-		do{
+		// Scan the text from left to right, 
+		// compare all the m-grams in text with the pattern from right to left
+		do{ // If the m-gram and pattern match at the last index
 			if(T.charAt(i) == P.charAt(j)){
-				if(j == 0){ 
-					result.add(i);
-					i = i + m;
+				if(j == 0){  // If the left bound of pattern is reached
+					result.add(i); // Add the shift index to the result list
+					i = i + m; 
 					j = m - 1;
 				}else{
-					j--; i--;
+					j--; i--; // If the left bound of pattern is not reached yet, keep moving to the left
 				}
-			} else{
-				i = i + m - min(j, 1 + last(P)[T.charAt(i) - '0']);
-				j = m - 1;
+			} else{ // If the m-gram and pattern does not match at the last index
+				i = i + m - min(j, 1 + last(P)[T.charAt(i) - '0']); // shift to the last occurred location of the current character in the next m-gram 
+				j = m - 1; // compare the new m-gram with pattern again
 			}
 
 		} while( i <= n - 1);
@@ -356,13 +380,12 @@ public class rbkStringMatcher {
 	 */
 	static int[] last(String P){
 
-		int j = P.length() - 1;
-
+		// Assume the alphabet for the pattern is {0,1}
+		// Find the last occurrence of character '0' and '1' in the pattern
 		int[] last = new int[2];
 
 		last[0] = last[1] = -1;
-
-		for(int i = 0; i <= j; i++){
+		for(int i = 0; i <= P.length() - 1; i++){
 			last[P.charAt(i) - '0'] = i;
 		}
 
@@ -379,29 +402,21 @@ public class rbkStringMatcher {
 		return (i < j)? i:j;
 	}
 
-
-	// Helper function to calculate the running time of each algorithm above
-	static long startTime, endTime, elapsedTime;
-	static int phase = 0;
-
-	static void rbkTimer() {
-		if(phase == 0) { startTime = System.currentTimeMillis();  phase = 1; }
-		else {
-			endTime = System.currentTimeMillis();  
-			elapsedTime = endTime-startTime;
-			System.out.print(elapsedTime + " ");
-			phase = 0;
-		}
-	}
-
-}
+} // end of rbkStringMatcher class
 
 
+// Helper class to calculate the running time of each algorithm
+/**
+ * The In class implements a driver to get input string from a text file.
+ * In the readLine method, it discards any newlines in the string.
+ * @author KAI
+ * 
+ */
 class In{
 
 	// read input file with a scanner
 	private Scanner scanner;
-	
+
 	// assume Unicode UTF-8 encoding
 	private static final String CHARSET_NAME = "UTF-8";
 
@@ -446,4 +461,5 @@ class In{
 	public void close() {
 		scanner.close();  
 	}
-}
+	
+} // end of In class
